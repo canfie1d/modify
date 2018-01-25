99270452950a4fcd29011d9c6c7c0a403e332aa5
Ryan <ryancanfield@me.com>
2018-01-24 17:25:43 -0800

    Form updates

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -6,15 +6,16 @@ export default class Contact extends React.Component {
       <div className='content-flex'>
         <p className='p p--dark m-b-50'>If you need anything at all, don't hesitate to send us a message and we'll get back to you as soon as we can!</p>
         <form className='form' name="contact" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
           <div className='form__column'>
             <label className='label' htmlFor='name'>Name</label>
-            <input className='input' id='name' />
-            <label className='label' htmlFor='title'>Subject</label>
-            <input className='input' id='title' />
+            <input className='input' name="name" id='name' type='text' required />
+            <label className='label' htmlFor='email'>Email</label>
+            <input className='input' name="email" id='email' type='email' required />
           </div>
           <div className='form__column m-b-50'>
             <label className='label' htmlFor='message'>Message</label>
-            <textarea className='textarea' rows={6} id='message' />
+            <textarea className='textarea' name='message' rows={6} id='message' required />
           </div>
           <div className='form__column'>
             <button type="submit" className='button button--alt'>Send</button>
