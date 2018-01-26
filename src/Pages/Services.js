5352caa06115ea9fded6f057881c1cd4824912e5
Ryan <ryancanfield@me.com>
2018-01-26 15:20:31 -0800

    Refactor service menu

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import ServiceMenu from '../Components/ServiceMenu';
-import { PRODUCTS } from '../Services/Data';
+import Products from '../Components/Products';
 
 export default class Services extends React.Component {
-
-  renderImages() {
-    let images = [];
-
-    for (let i = 0; i < PRODUCTS.length; i++) {
-      images.push(
-        <div key={`brand-image-${i}`} className='product__img-wrapper'>
-          <img className='products__img' alt={PRODUCTS[i].alt} src={PRODUCTS[i].src} />
-        </div>
-      );
-    }
-
-    return images;
-
-  }
-
   render() {
     return (
       <div className='content-flex'>
         <p className='p p--dark m-b-100'>All of our stylists are independent contractors and prices vary dependent on each individual stylist. To see each stylist specific price details, click on the stylist bio tab.</p>
-        <ServiceMenu {...this.props} />
-        <p className='p p--dark p--small p--align-left'>* Hana only</p>
-        <p className='p p--dark p--small p--align-left m-b-100'>** Tina &amp; Alysia only</p>
-        <div className='products'>
-          {this.renderImages()}
-        </div>
+        <ServiceMenu />
+        <Products />
       </div>
     );
   }
