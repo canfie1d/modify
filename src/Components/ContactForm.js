d66169071cee8ca12fdd5304bb214e0d8ab7d9dd
Ryan <ryancanfield@me.com>
2018-02-10 11:23:13 -0800

    Form style update

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -26,6 +26,14 @@ export default class ContactForm extends React.Component {
       .then(() => this.props.formSent())
       .catch(error => this.props.formSent(error));
 
+    if (this.props.formSent && this.props.formError === null) {
+      this.setState({
+        name: "",
+        email: "",
+        message: ""
+      });
+    }
+
     e.preventDefault();
   };
 
@@ -38,16 +46,43 @@ export default class ContactForm extends React.Component {
       <form className='form' onSubmit={this.handleSubmit}>
         <div className='form__column'>
           <label className='label' htmlFor='name'>Name</label>
-          <input className='input' name="name" id='name' type='text' required value={this.state.name} onChange={this.handleChange} />
+          <input
+            id='name'
+            name='name'
+            type='text'
+            className='input'
+            placeholder="What is your name?"
+            value={this.state.name}
+            onChange={this.handleChange}
+            required
+          />
           <label className='label' htmlFor='email'>Email</label>
-          <input className='input' name="email" id='email' type='email' required value={this.state.email} onChange={this.handleChange} />
+          <input
+            id='email'
+            name='email'
+            type='email'
+            className='input'
+            placeholder="What is your email? (we keep this very private)"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
         </div>
         <div className='form__column m-b-50'>
           <label className='label' htmlFor='message'>Message</label>
-          <textarea className='textarea' name='message' rows={6} id='message' required value={this.state.message} onChange={this.handleChange} />
+          <textarea
+            id='message'
+            name='message'
+            className='textarea'
+            rows={5}
+            placeholder="How can we help?"
+            value={this.state.message}
+            onChange={this.handleChange}
+            required
+          />
         </div>
         <div className='form__column'>
-          <button type="submit" className='button button--alt'>Send</button>
+          <button type='submit' className='button button--alt'>Send</button>
         </div>
       </form>
     );
