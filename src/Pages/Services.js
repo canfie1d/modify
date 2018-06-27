a6da1041255f17976467836194d30ca51d6389b8
Ryan <ryancanfield@me.com>
2018-06-26 22:44:26 -0700

    Typographic tweaks

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -6,7 +6,7 @@ export default class Services extends React.Component {
   render() {
     return (
       <div className='content-flex'>
-        <p className='p p--dark m-b-100'>All of our stylists are independent contractors and prices vary dependent on each individual stylist. To see each stylists specific price details, view their <Link to='/stylists'>profiles</Link>.</p>
+        <p className='p p--dark m-b-100'>All of our stylists are independent contractors and their pricing is set individually. To see each stylists specific price details, view their <Link to='/stylists'>profiles</Link>.</p>
         <ServiceMenu />
         <NavLink className='nav-link__button nav-link__button--alt' to='/products'>View Our Products</NavLink>
       </div>
