3ab01792f7322a0ec67d9cc7383aa1a5e1528562
Ryan <ryancanfield@me.com>
2018-06-26 20:40:11 -0700

    Update services data

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ServiceMenu from '../Components/ServiceMenu';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 export default class Services extends React.Component {
   render() {
     return (
       <div className='content-flex'>
-        <p className='p p--dark m-b-100'>All of our stylists are independent contractors and prices vary dependent on each individual stylist. To see each stylist specific price details, click on the stylist bio tab.</p>
+        <p className='p p--dark m-b-100'>All of our stylists are independent contractors and prices vary dependent on each individual stylist. To see each stylists specific price details, view their <Link to='/stylists'>profiles</Link>.</p>
         <ServiceMenu />
         <NavLink className='nav-link__button nav-link__button--alt' to='/products'>View Our Products</NavLink>
       </div>
